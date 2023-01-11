66d177aee0ddfbeadfee8e3e788354159ac5c6db
unknown <dani.07rojas94@gmail.com>
2023-01-10 21:39:23 -0500

    Dapoos V.1.0

---
+++ src/Presentation/JailGUI.java
@@ -0,0 +1,9 @@
+package Presentation;
+
+import java.awt.Color;
+
+public class JailGUI extends CasillaGUI{
+	public JailGUI(){
+        setBackground(Color.BLACK);
+    }
+    }
---
+++ src/Presentation/MineGUI.java
@@ -0,0 +1,12 @@
+package Presentation;
+
+import java.awt.Color;
+import java.awt.*;
+
+public class MineGUI extends CasillaGUI{
+    public MineGUI() {
+        setBackground(Color.YELLOW);
+        setLayout(new GridLayout(1,1));
+    }
+
+}
